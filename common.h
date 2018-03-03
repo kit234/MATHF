c27703a66e7a2a6f0466297db8b7761f78202395
kit234 <36580946+kit234@users.noreply.github.com>
2018-03-03 13:06:20 +0800

    包含其他文件公共函数的头文件

---
+++ common.h
@@ -0,0 +1,9 @@
+//common.h：多个文件需要用到的公共函数的声明
+#ifndef COMMON_H
+#define COMMON_H
+#include <string>
+
+std::string DeleteBlank(std::string);		//删除字符串中的空格
+std::string GetTypeOf(std::string);			//获取数据类型
+
+#endif
