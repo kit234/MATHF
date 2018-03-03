bacd61820cda8a95ab9c9b792e5f914acc64cf5c
kit234 <36580946+kit234@users.noreply.github.com>
2018-03-03 13:03:56 +0800

    包含所有类型运算的头文件

---
+++ operation.h
@@ -0,0 +1,9 @@
+//operation.h：声明和运算有关的函数
+#ifndef OPERATION_H
+#define OPERATION_H
+#include <string>
+
+std::string operation_number(std::string);	//数字型运算函数
+std::string operation_string(std::string);	//字符串型运算函数	
+
+#endif
